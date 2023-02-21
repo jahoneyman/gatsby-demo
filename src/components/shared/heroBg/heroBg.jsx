860f6041d90563dba3a987f5159da31d4056048b
John Honeyman <johnhoneyman10@gmail.com>
2023-02-21 17:55:22 +0800

    Changed hero video background

--- src/components/shared/heroBg/heroBg.jsx
+++ src/components/shared/heroBg/heroBg.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import Hero from '../../../assets/videos/hero.mp4';
+import React, { useState, useEffect, useRef } from 'react'
+
+import Hero from '../../../assets/videos/hero.webm';
 
 const HeroBg = () => {
+
     return (
         <video src={Hero} autoPlay loop muted className='w-full h-full object-cover' />
     )
