c865d2be965dd2120a1e96c590f00899c04a3059
John Honeyman <johnhoneyman10@gmail.com>
2023-02-22 11:45:17 +0800

    Initial features images commit
    Added images for features section

--- src/components/pages/home/features/features.tsx
+++ src/components/pages/home/features/features.tsx
@@ -1,25 +1,41 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
-import Heading from "../../../shared/heading";
 import FEATURES from "../../../../constants/features";
+import Heading from "../../../shared/heading";
 
 const Features = () => {
+  const data = useStaticQuery(graphql`
+    query Images {
+      images: allImageSharp {
+        nodes {
+          id
+          fluid {
+            src
+            srcSet
+          }
+        }
+      }
+    }
+  `);
+
   const featuresBody = FEATURES.features;
-  console.log(featuresBody);
 
   return (
     <section className="h-screen flex items-center justify-center bg-gray-1">
       <div className="text-center w-[1260px]">
-        <Heading className="text-primary-1" tag="h1" size="xl">
+        <Heading className="text-primary-1 my-14" tag="h1" size="xl">
           <h1>Meet your new</h1>
           <h1>all-in-one money app</h1>
         </Heading>
         <ul className="flex justify-evenly">
-          {featuresBody.map(({ name, description, image }, index) => {
+          {featuresBody.map(({ name, description, image: imgName, alt_image }, index) => {
+            const image = "./images/" + imgName;
+
             return (
               <li key={index} className="w-1/4 px-4">
-                <div>{image}</div>
+                {/* <StaticImage src="" alt={alt_image} /> */}
                 <h1 className="text-primary-1 text-4xl font-bold pb-4">{name}</h1>
                 <p className="text-white pb-8">{description}</p>
               </li>
--- src/constants/features.ts
+++ src/constants/features.ts
@@ -2,25 +2,34 @@ interface IFeatures {
   name: string;
   description: string;
   image: string;
+  alt_image: string;
 }
 
 const features: IFeatures[] = [
-  { name: "Send", description: "A cooler way to send money is here with @username", image: "" },
+  {
+    name: "Send",
+    description: "A cooler way to send money is here with @username",
+    image: "home_send.png",
+    alt_image: "image of send icon",
+  },
   {
     name: "Shop",
     description:
       "Shop online and in-store from our wide variety of merchants and community of verified sellers",
-    image: "",
+    image: "home_spend.png",
+    alt_image: "image of shop icon",
   },
   {
     name: "Save",
     description: "Rest easy with your savings tucked away in a BSP regulated bank",
-    image: "",
+    image: "save.png",
+    alt_image: "image of save icon",
   },
   {
     name: "Grow",
     description: "Want to invest in crypto? With Maya you can grow your money how you like it",
-    image: "",
+    image: "invest.png",
+    alt_image: "image of grow icon",
   },
 ];
 
