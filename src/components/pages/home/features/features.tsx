42b7b65e745b831f4cbd402eda09fd1c4edc3c8b
John Honeyman <johnhoneyman10@gmail.com>
2023-02-22 16:04:11 +0800

    Added features icon to features section.

--- src/components/pages/home/features/features.tsx
+++ src/components/pages/home/features/features.tsx
@@ -1,41 +1,24 @@
 import React from "react";
-import { Link, graphql, useStaticQuery } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
 
 import FEATURES from "../../../../constants/features";
 import Heading from "../../../shared/heading";
 
 const Features = () => {
-  const data = useStaticQuery(graphql`
-    query Images {
-      images: allImageSharp {
-        nodes {
-          id
-          fluid {
-            src
-            srcSet
-          }
-        }
-      }
-    }
-  `);
-
   const featuresBody = FEATURES.features;
 
   return (
-    <section className="h-screen flex items-center justify-center bg-gray-1">
+    <section className="h-[1000px] flex items-center justify-center bg-gray-1">
       <div className="text-center w-[1260px]">
-        <Heading className="text-primary-1 my-14" tag="h1" size="xl">
-          <h1>Meet your new</h1>
-          <h1>all-in-one money app</h1>
+        <Heading className="text-primary-1 mb-14" tag="h1" size="xl">
+          <p>Meet your new</p>
+          <p>all-in-one money app</p>
         </Heading>
         <ul className="flex justify-evenly">
-          {featuresBody.map(({ name, description, image: imgName, alt_image }, index) => {
-            const image = "./images/" + imgName;
-
+          {featuresBody.map(({ name, description, image, alt_image }, index) => {
             return (
               <li key={index} className="w-1/4 px-4">
-                {/* <StaticImage src="" alt={alt_image} /> */}
+                <img src={image} alt={alt_image} className="mx-auto" />
                 <h1 className="text-primary-1 text-4xl font-bold pb-4">{name}</h1>
                 <p className="text-white pb-8">{description}</p>
               </li>
--- src/constants/features.ts
+++ src/constants/features.ts
@@ -1,3 +1,8 @@
+import home_send from "../assets/images/features/home_send.png";
+import home_spend from "../assets/images/features/home_spend.png";
+import saveIcon from "../assets/images/features/save.png";
+import investIcon from "../assets/images/features/invest.png";
+
 interface IFeatures {
   name: string;
   description: string;
@@ -9,26 +14,26 @@ const features: IFeatures[] = [
   {
     name: "Send",
     description: "A cooler way to send money is here with @username",
-    image: "home_send.png",
+    image: home_send,
     alt_image: "image of send icon",
   },
   {
     name: "Shop",
     description:
       "Shop online and in-store from our wide variety of merchants and community of verified sellers",
-    image: "home_spend.png",
+    image: home_spend,
     alt_image: "image of shop icon",
   },
   {
     name: "Save",
     description: "Rest easy with your savings tucked away in a BSP regulated bank",
-    image: "save.png",
+    image: saveIcon,
     alt_image: "image of save icon",
   },
   {
     name: "Grow",
     description: "Want to invest in crypto? With Maya you can grow your money how you like it",
-    image: "invest.png",
+    image: investIcon,
     alt_image: "image of grow icon",
   },
 ];
